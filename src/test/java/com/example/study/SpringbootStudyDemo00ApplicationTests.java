debe4275204760b8cd87339e2b2accb7d605f4d6
ataraxy-zhi <githubjiang0313>
2022-11-19 12:40:50 +0800

    init

---
+++ src/test/java/com/example/study/SpringbootStudyDemo00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyDemo00ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
